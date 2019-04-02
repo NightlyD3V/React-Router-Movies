11a35ffdd256ac31107c7e7df683c1aebf5d728e
NightlyD3V <TylerSpaulding95@gmail.com>
2019-04-02 20:52:23 +0100

    Completed stretch save functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -11,6 +11,7 @@ export default class Movie extends Component {
 
   componentDidMount() {
     // change this line to grab the id passed on the URL
+    console.log(this.props);
     const id = this.props.match.params.id;
     console.log(id);
     this.fetchMovie(id);
@@ -27,23 +28,24 @@ export default class Movie extends Component {
       });
   };
   // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
+  componentWillReceiveProps(newProps){
+    if(this.props.match.params.id !== newProps.match.params.id){
+      this.fetchMovie(newProps.match.params.id);
+    }
+  }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie)
+  }
 
-  render() {
+  render(props) {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
     const { title, director, metascore, stars } = this.state.movie;
+    console.log(props);
     return (
       <div className="save-wrapper">
         <div className="movie-card">
@@ -62,7 +64,7 @@ export default class Movie extends Component {
             </div>
           ))}
         </div>
-        <div className="save-button">Save</div>
+        <div onClick={this.saveMovie} className="save-button">Save</div>
       </div>
     );
   }
